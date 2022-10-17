23402780abec6f3b2cedf9bfc4cb66a52d33d9ae
AmayuTI21A <114399243+AmayuTI21A@users.noreply.github.com>
2022-10-17 21:31:58 +0700

    Add files via upload

---
+++ Uchiha.java
@@ -0,0 +1,12 @@
+class Uchiha extends Clan{
+   Uchiha() {
+      System.out.println("_______________________________");
+    }
+protected void AnggotaClan() {
+        System.out.println("Uchiha Sasuke");
+    }
+public static void main (String[] args) {
+    Uchiha Uchiha = new Uchiha();
+        Uchiha.AnggotaClan();
+    }
+}
