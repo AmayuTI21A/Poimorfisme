32f7bc90fccf7da9b93613419ace082bf8babf07
AmayuTI21A <114399243+AmayuTI21A@users.noreply.github.com>
2022-10-17 21:30:49 +0700

    Add files via upload

---
+++ Uzumaki.java
@@ -0,0 +1,17 @@
+class Uzumaki extends Clan {
+
+    Uzumaki() {
+        System.out.println("_______________________________");
+    }
+
+   
+    protected void AnggotaClan() {
+        
+        System.out.println("Naruto Uzumaki");
+    }
+    public static void main (String[] args) {
+        Uzumaki Uzumaki = new Uzumaki();
+        Uzumaki.AnggotaClan();
+    }
+
+}
